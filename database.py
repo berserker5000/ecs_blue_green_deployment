ece15a12e4258eb2b6e430915f54ef56108f82ff
Dmytro <kulyk.dmitry@gmail.com>
2024-03-19 15:13:38 +0100

    v1

--- database.py
+++ database.py
@@ -107,6 +107,7 @@ def delete_rds_and_deployment(client, deployment_id: str, rds_arn: str):
 		while status != "SWITCHOVER_COMPLETED":
 			print(f"Checking for switchover is finished. Status: {status}")
 			time.sleep(10)
+			description = client.describe_blue_green_deployments(BlueGreenDeploymentIdentifier=deployment_id)
 			status = description["BlueGreenDeployments"][0]["Status"]
 		print(f"Deleting deployment {deployment_id}")
 		client.delete_blue_green_deployment(BlueGreenDeploymentIdentifier=deployment_id, DeleteTarget=False)
